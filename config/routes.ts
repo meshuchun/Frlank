e038dd7386cddbb3df8a987cb89a05302b07cde1
FranckRain <sharkthought@gmail.com>
2022-04-01 09:01:05 +0800

    Add Project Templates

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,4 @@
-﻿export default [
+export default [
   {
     path: '/user',
     layout: false,
@@ -10,6 +10,18 @@
             name: 'login',
             path: '/user/login',
             component: './user/Login',
+          },
+          {
+            name: '注册结果页',
+            icon: 'smile',
+            path: '/userregisterresult',
+            component: './UserRegisterResult',
+          },
+          {
+            name: '注册页',
+            icon: 'smile',
+            path: '/userregister',
+            component: './UserRegister',
           },
         ],
       },
@@ -43,6 +55,36 @@
     ],
   },
   {
+    name: '卡片列表',
+    icon: 'smile',
+    path: '/listcardlist',
+    component: './ListCardList',
+  },
+  {
+    name: '搜索列表（文章）',
+    icon: 'smile',
+    path: '/listsearcharticles',
+    component: './ListSearchArticles',
+  },
+  {
+    name: '搜索列表（项目）',
+    icon: 'smile',
+    path: '/listsearchprojects',
+    component: './ListSearchProjects',
+  },
+  {
+    name: '搜索列表（应用）',
+    icon: 'smile',
+    path: '/listsearchapplications',
+    component: './ListSearchApplications',
+  },
+  {
+    name: '搜索列表',
+    icon: 'smile',
+    path: '/listsearch',
+    component: './ListSearch',
+  },
+  {
     name: 'list.table-list',
     icon: 'table',
     path: '/list',
@@ -51,6 +93,18 @@
   {
     path: '/',
     redirect: '/welcome',
+  },
+  {
+    name: '个人中心',
+    icon: 'smile',
+    path: '/accountcenter',
+    component: './AccountCenter',
+  },
+  {
+    name: '个人设置',
+    icon: 'smile',
+    path: '/accountsettings',
+    component: './AccountSettings',
   },
   {
     component: './404',
